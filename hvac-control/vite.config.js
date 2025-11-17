42001617d980127436454a9546cee879092817a0
natebarksdale <natebarksdale@gmail.com>
2025-11-17 16:45:16 -0500

    Add HVAC control app build for deployment

--- hvac-control/vite.config.js
+++ hvac-control/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/hvac-control/' : '/',
+  base: mode === 'production' ? '/experiments/hvac-control/' : '/',
 }))
